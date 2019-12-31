d7a61edab17d39e41c7191d31d2a89c401bb350d
flack.chen <flack.chen@icmofang.com>
2019-12-31 11:08:19 +0800

    增加mypillow项目

---
+++ MyPillow/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+@File           : __init__.py.py
+@Time           : 2019/12/31 8:51
+@Author         : Flack
+@Email          : opencoding@hotmail.com
+@ide            : PyCharm
+@project        : mytest
+@description    : 描述
+"""
