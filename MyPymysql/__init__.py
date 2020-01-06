5278c4bdac7883c1c31fb5d1bf7f542e1f18eccf
flack.chen <flack.chen@icmofang.com>
2020-01-06 21:56:39 +0800

    提交

---
+++ MyPymysql/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+@File           : __init__.py.py
+@Time           : 2020/1/6 21:52
+@Author         : Flack
+@Email          : opencoding@hotmail.com
+@ide            : PyCharm
+@project        : mytest
+@description    : 描述
+"""
---
+++ MyPymysql/test.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+"""
+@File           : test.py
+@Time           : 2020/1/6 21:53
+@Author         : Flack
+@Email          : opencoding@hotmail.com
+@ide            : PyCharm
+@project        : mytest
+@description    : 描述
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
